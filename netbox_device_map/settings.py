63db786a62442d085f20a2280140354046ef17a0
ichha <thapa.ichha@gmail.com>
2025-11-08 01:24:46 +0545

    Update settings.py

--- netbox_device_map/settings.py
+++ netbox_device_map/settings.py
@@ -1,7 +1,9 @@
 from django.conf import settings
-
 from . import config
 
-# Overlay custom settings over default settings
-plugin_settings = config.default_settings | settings.PLUGINS_CONFIG[config.name]
-plugin_settings['geomap_settings'] = config.default_settings['geomap_settings'] | plugin_settings['geomap_settings']
+user_config = settings.PLUGINS_CONFIG.get(config.name, {})
+plugin_settings = config.default_settings | user_config
+plugin_settings['geomap_settings'] = (
+    config.default_settings['geomap_settings'] | 
+    plugin_settings.get('geomap_settings', {})
+)
