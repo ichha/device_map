26b5f728d3e518be1690ba3d9728427c631ec4d8
ichha <thapa.ichha@gmail.com>
2025-11-08 01:25:17 +0545

    Update navigation.py

--- netbox_device_map/navigation.py
+++ netbox_device_map/navigation.py
@@ -1,9 +1,12 @@
 from netbox.plugins import PluginMenuItem
-
+from netbox.navigation import create_menu_items
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_device_map:map',
-        link_text='Device map',
+        link='plugins:device_map:map',
+        link_text='Device Map',
+        permissions=('dcim.view_device',),
     ),
 )
+
+create_menu_items('plugins', menu_items)
