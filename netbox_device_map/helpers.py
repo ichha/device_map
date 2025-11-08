14bfee5470b2db43e1306adeba9dfcec7b97400b
ichha <thapa.ichha@gmail.com>
2025-11-08 06:58:34 +0545

    Update helpers.py

--- netbox_device_map/helpers.py
+++ netbox_device_map/helpers.py
@@ -1,44 +1,44 @@
-from typing import Tuple, Optional
 from packaging import version
+
 from dcim.models import Device
 from django.db.models import QuerySet, Q
 from ipam.models import VLAN
 from netbox.settings import VERSION
+
 from .settings import plugin_settings
 
-LOCATION_CF_NAME = plugin_settings.get('device_geolocation_cf', 'geolocation')
-NETBOX_VERSION = version.parse(VERSION.split('-')[0])
-LatLon = Tuple[float, float]
 
-def get_device_location(device: Device) -> Optional[LatLon]:
-    if not device:
-        return None
-    location_cf = device.custom_field_data.get(LOCATION_CF_NAME)
-    if not location_cf:
-        return None
-    try:
-        lat, lon = map(float, location_cf.replace(' ', '').split(',', maxsplit=1))
-        return lat, lon
-    except (ValueError, AttributeError):
-        return None
+LOCATION_CF_NAME = plugin_settings['device_geolocation_cf']
+NETBOX_VERSION = version.parse(VERSION)
+LatLon = tuple[float, float]
 
-def get_connected_devices(device: Device, vlan: Optional[VLAN] = None) -> QuerySet[Device]:
-    if not device:
-        return Device.objects.none()
-    interfaces = device.interfaces.all()
+
+def get_device_location(device: Device) -> LatLon | None:
+    """Extract device geolocation from special custom field"""
+    if location_cf := device.custom_field_data.get(LOCATION_CF_NAME):
+        return tuple(map(float, location_cf.replace(' ', '').split(',', maxsplit=1)))
+
+
+def get_connected_devices(device: Device, vlan: VLAN = None) -> QuerySet[Device]:
+    """Get list of connected devices to the specified device.
+    If the vlan is specified, return only devices connected to the interfaces of the specified device
+    containing the specified VLAN"""
+    included_interfaces = device.interfaces.all()
     if vlan is not None:
-        interfaces = interfaces.filter(
-            Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan)
-        )
-    connected_devices = Device.objects.filter(
-        interfaces__cable__terminations__interface__in=interfaces
-    ).exclude(pk=device.pk).distinct()
-    return connected_devices
+        included_interfaces = included_interfaces.filter(Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan))
+    if NETBOX_VERSION < version.parse('3.3.0'):
+        return Device.objects.filter(interfaces___link_peer_id__in=included_interfaces)
+    else:
+        return Device.objects.filter(
+            interfaces__cable__terminations__interface__in=device.interfaces.all()
+        ).exclude(pk=device.id)
+
 
 def are_devices_connected(device_a: Device, device_b: Device) -> bool:
-    if not device_a or not device_b:
-        return False
-    return Device.objects.filter(
-        pk=device_b.pk,
-        interfaces__cable__terminations__interface__in=device_a.interfaces.all(),
-    ).exists()
+    """Determines whether devices are connected to each other by a direct connection"""
+    if NETBOX_VERSION < version.parse('3.3.0'):
+        queryset = Device.objects.filter(interfaces___link_peer_id__in=device_a.interfaces.all(), id=device_b.id)
+    else:
+        queryset = Device.objects.filter(
+            interfaces__cable__terminations__interface__in=device_a.interfaces.all(), id=device_b.id)
+    return bool(queryset.values('pk'))
