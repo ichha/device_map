fab0a18431d5dcda58699b4d996725985ac4a016
ichha <thapa.ichha@gmail.com>
2025-11-08 07:45:45 +0545

    Update helpers.py

--- netbox_device_map/helpers.py
+++ netbox_device_map/helpers.py
@@ -1,59 +1,38 @@
-from typing import Optional, Tuple
-from packaging import version
-
-from dcim.models import Device
-from django.db.models import QuerySet, Q
+from dcim.models import Device, Site
+from django.db.models import Q
 from ipam.models import VLAN
+from packaging import version
 from netbox.settings import VERSION
-
 from .settings import plugin_settings
 
-
-# --- Constants ---
 LOCATION_CF_NAME = plugin_settings['device_geolocation_cf']
-
-# Fix invalid NetBox Docker version like '4.4.4-Docker-3.4.1'
-NETBOX_VERSION = version.parse(VERSION.split('-')[0])
-
-LatLon = Tuple[float, float]
+NETBOX_VERSION = version.parse(VERSION)
+LatLon = tuple[float, float]
 
 
-# --- Functions ---
-
-def get_device_location(device: Device) -> Optional[LatLon]:
-    """Extract device geolocation from custom field as (lat, lon)."""
+def get_device_location(device: Device) -> LatLon | None:
+    """Extract device geolocation from custom field or site location."""
+    # Try from custom field first
     if location_cf := device.custom_field_data.get(LOCATION_CF_NAME):
         try:
             return tuple(map(float, location_cf.replace(' ', '').split(',', maxsplit=1)))
-        except (ValueError, AttributeError):
-            return None
+        except Exception:
+            pass
+    # Then try from site (if available)
+    if hasattr(device, 'site') and getattr(device.site, 'latitude', None) and getattr(device.site, 'longitude', None):
+        return (device.site.latitude, device.site.longitude)
     return None
 
 
-def get_connected_devices(device: Device, vlan: Optional[VLAN] = None) -> QuerySet[Device]:
-    """
-    Get list of connected devices to the specified device.
-    If VLAN is specified, only include devices whose interfaces use that VLAN.
-    """
+def get_connected_devices(device: Device, vlan: VLAN = None):
+    """Get list of connected devices to the specified device."""
     included_interfaces = device.interfaces.all()
     if vlan is not None:
-        included_interfaces = included_interfaces.filter(
-            Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan)
-        )
+        included_interfaces = included_interfaces.filter(Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan))
 
-    # NetBox 3.3+ uses cables and terminations for interface links
-    return (
-        Device.objects.filter(
-            interfaces__cable__terminations__interface__in=included_interfaces
-        )
-        .exclude(pk=device.id)
-        .distinct()
-    )
-
-
-def are_devices_connected(device_a: Device, device_b: Device) -> bool:
-    """Check if two devices are directly connected by a cable."""
-    return Device.objects.filter(
-        interfaces__cable__terminations__interface__in=device_a.interfaces.all(),
-        pk=device_b.pk,
-    ).exists()
+    if NETBOX_VERSION < version.parse('3.3.0'):
+        return Device.objects.filter(interfaces___link_peer_id__in=included_interfaces)
+    else:
+        return Device.objects.filter(
+            interfaces__cable__terminations__interface__in=device.interfaces.all()
+        ).exclude(pk=device.id)
